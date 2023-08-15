d3cb6b219546eeeedc621e58afe4c1dbaa2a962e
Jos-bases <142135994+Jos-bases@users.noreply.github.com>
2023-08-15 09:49:28 +0800

    Update time.go
    update

--- tool/time.go
+++ tool/time.go
@@ -35,16 +35,31 @@ func (t *TimeTool) LoadLocation(loc TimeLocation) *TimeTool  {
 
 func (t *TimeTool) AddDay(day int) *TimeTool  {
 
+	if t.format == "" {
+		t.format = Y_M_D_H_I_S
+	}
+
 	if t.tm == nil {
 		if t.loc != nil {
-			tm, _ := time.ParseInLocation(string(t.format), t.timeStr, t.loc)
-			tm = tm.AddDate(0,0, day)
-			t.tm = &tm
+			if t.timeStr != "" {
+				tm, _ := time.ParseInLocation(string(t.format), t.timeStr, t.loc)
+				tm = tm.AddDate(0,0, day)
+				t.tm = &tm
+			} else {
+				tm := time.Now().AddDate(0,0, day)
+				t.tm = &tm
+			}
+
 
 		} else {
-			tm, _ := time.Parse(string(t.format), t.timeStr)
-			tm = tm.AddDate(0,0, day)
-			t.tm = &tm
+			if t.timeStr != "" {
+				tm, _ := time.Parse(string(t.format), t.timeStr)
+				tm = tm.AddDate(0,0, day)
+				t.tm = &tm
+			} else {
+				tm := time.Now().AddDate(0,0, day)
+				t.tm = &tm
+			}
 
 		}
 	} else {
@@ -59,16 +74,32 @@ func (t *TimeTool) AddDay(day int) *TimeTool  {
 
 func (t *TimeTool) AddMonth(month int) *TimeTool {
 
+	if t.format == "" {
+		t.format = Y_M_D_H_I_S
+	}
+
 	if t.tm == nil {
 		if t.loc != nil {
-			tm, _ := time.ParseInLocation(string(t.format), t.timeStr, t.loc)
-			tm = tm.AddDate(0,month, 0)
-			t.tm = &tm
+			if t.timeStr != "" {
+				tm, _ := time.ParseInLocation(string(t.format), t.timeStr, t.loc)
+				tm = tm.AddDate(0,month, 0)
+				t.tm = &tm
+			} else {
+				tm := time.Now().AddDate(0,month, 0)
+				t.tm = &tm
+			}
+
 
 		} else {
-			tm, _ := time.Parse(string(t.format), t.timeStr)
-			tm = tm.AddDate(0,month, 0)
-			t.tm = &tm
+			if t.timeStr != "" {
+				tm, _ := time.Parse(string(t.format), t.timeStr)
+				tm = tm.AddDate(0,month, 0)
+				t.tm = &tm
+			} else {
+				tm := time.Now().AddDate(0,month, 0)
+				t.tm = &tm
+			}
+
 		}
 	} else {
 		tm := t.tm.AddDate(0,month, 0)
@@ -80,16 +111,32 @@ func (t *TimeTool) AddMonth(month int) *TimeTool {
 
 func (t *TimeTool) AddYear(year int) *TimeTool  {
 
+	if t.format == "" {
+		t.format = Y_M_D_H_I_S
+	}
+
 	if t.tm == nil {
 		if t.loc != nil {
-			tm, _ := time.ParseInLocation(string(t.format), t.timeStr, t.loc)
-			tm = tm.AddDate(year,0, 0)
-			t.tm = &tm
+			if t.timeStr != "" {
+				tm, _ := time.ParseInLocation(string(t.format), t.timeStr, t.loc)
+				tm = tm.AddDate(year,0, 0)
+				t.tm = &tm
+			} else {
+				tm := time.Now().In(t.loc).AddDate(year,0, 0)
+				t.tm = &tm
+			}
+
 
 		} else {
-			tm, _ := time.Parse(string(t.format), t.timeStr)
-			tm = tm.AddDate(year,0, 0)
-			t.tm = &tm
+			if t.timeStr != "" {
+				tm, _ := time.Parse(string(t.format), t.timeStr)
+				tm = tm.AddDate(year,0, 0)
+				t.tm = &tm
+			} else {
+				tm := time.Now().AddDate(year,0, 0)
+				t.tm = &tm
+			}
+
 		}
 	} else {
 		tm := t.tm.AddDate(year,0, 0)
@@ -101,16 +148,31 @@ func (t *TimeTool) AddYear(year int) *TimeTool  {
 
 func (t *TimeTool) AddSecond(second int) *TimeTool  {
 
+	if t.format == "" {
+		t.format = Y_M_D_H_I_S
+	}
+
 	if t.tm == nil {
 		if t.loc != nil {
-			tm, _ := time.ParseInLocation(string(t.format), t.timeStr, t.loc)
-			tm = tm.Add(time.Duration(second))
-			t.tm = &tm
+			if t.timeStr != "" {
+				tm, _ := time.ParseInLocation(string(t.format), t.timeStr, t.loc)
+				tm = tm.Add(time.Duration(second))
+				t.tm = &tm
+			} else {
+				tm := time.Now().In(t.loc).Add(time.Duration(second))
+				t.tm = &tm
+			}
 
 		} else {
-			tm, _ := time.Parse(string(t.format), t.timeStr)
-			tm = tm.Add(time.Duration(second))
-			t.tm = &tm
+			if t.timeStr != "" {
+				tm, _ := time.Parse(string(t.format), t.timeStr)
+				tm = tm.Add(time.Duration(second))
+				t.tm = &tm
+			} else {
+				tm := time.Now().Add(time.Duration(second))
+				t.tm = &tm
+			}
+
 		}
 	} else {
 		tm := t.tm.Add(time.Duration(second))
@@ -120,25 +182,70 @@ func (t *TimeTool) AddSecond(second int) *TimeTool  {
 	return t
 }
 
-func (t *TimeTool) GetTime() time.Time  {
+func (t *TimeTool) GetTime() (tm time.Time)  {
 
-	var tm time.Time
+	if t.format == "" {
+		t.format = Y_M_D_H_I_S
+	}
+
 	if t.loc != nil {
-		tm = t.tm.In(t.loc)
+		if t.tm == nil {
+			if t.timeStr != "" {
+
+				tm, _  = time.ParseInLocation(string(t.format), t.timeStr, t.loc)
+			} else {
+				tm = time.Now().In(t.loc)
+			}
+
+		} else {
+			tm = t.tm.In(t.loc)
+		}
+
 	} else {
-		tm = *t.tm
+		if t.tm == nil {
+			if t.timeStr != "" {
+				tm, _  = time.Parse( string(t.format), t.timeStr)
+			} else {
+				tm = time.Now()
+			}
+
+		} else {
+			tm = *t.tm
+		}
 	}
 
 	return tm
 }
 
-func (t *TimeTool) GetString() string  {
+func (t *TimeTool) GetString() (tm string) {
 
-	var tm string
-	if t.loc != nil {
-		tm = t.tm.In(t.loc).Format(string(t.format))
-	} else {
-		tm = t.tm.Format(string(t.format))
+	if t.format == "" {
+		t.format = Y_M_D_H_I_S
 	}
+
+	if t.loc != nil {
+		if t.tm == nil {
+			if t.timeStr != "" {
+				tm  = t.timeStr
+			} else {
+				tm = time.Now().In(t.loc).Format(string(t.format))
+			}
+		} else {
+			tm = t.tm.In(t.loc).Format(string(t.format))
+		}
+
+	} else {
+
+		if t.tm == nil {
+			if t.timeStr != "" {
+				tm  = t.timeStr
+			} else {
+				tm = time.Now().Format(string(t.format))
+			}
+		} else {
+			tm = t.tm.Format(string(t.format))
+		}
+	}
+
 	return tm
 }
